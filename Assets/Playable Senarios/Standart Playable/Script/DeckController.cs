43a7e95662d57b47f8479578375c5a3e452fcc9c
Gültekin Gültaş <158182394+gultekingultasceng@users.noreply.github.com>
2025-02-24 11:50:33 +0300

    fix

--- Assets/Playable Senarios/Standart Playable/Script/DeckController.cs
+++ Assets/Playable Senarios/Standart Playable/Script/DeckController.cs
@@ -11,7 +11,7 @@ public class DeckController : CardPile
         {
             var card = CardLayer.Instance.cardPool.GetCard();
             Add(card, i);
-            card.No = deckCardNoList[^(i+1)];
+            card.No = deckCardNoList[deckCardNoList.Count - 1 - i];
             Slots[i].transform.localPosition = offset * i;
             card.transform.SetParent(Slots[i]);
             card.transform.localPosition = Vector3.zero;
